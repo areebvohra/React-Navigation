d60565b9522e83f71c7fe0e88cfdabf48e459587
AreebVohra <areebvohra@gmail.com>
2019-06-30 19:39:21 +0500

    --commit : Detail Screen Added

---
+++ src/screens/Details/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => {
+            this.props.navigation.navigate('Home')
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+
+export default DetailsScreen;
